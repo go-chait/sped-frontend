e7724fadfcbfa732bb178a1ae73e8a5875d5d36b
Akshay Varma <akshayvarma@Akshays-MacBook-Air.local>
2024-06-28 13:24:10 -0400

    initial commit

---
+++ src/components/Login.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "../App.css";
+
+const Login = ({ onLogin }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onLogin(email, password);
+  };
+
+  return (
+    <div>
+      <h2>Admin Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>Email:</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
