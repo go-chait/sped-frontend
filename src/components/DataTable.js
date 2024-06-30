26ea9beb63395211b6d4e396a17982d276418312
= <mounikapoddaturi94@gmail.com>
2024-06-30 17:02:47 -0400

    calling api to fill the data table

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "../App.css";
 
-const DataTable = ({ data }) => {
+function App() {
+  const [data, setData] = useState([]);
+
+  const DataTable = async () => {
+    const url = "http://localhost:8000/data/getAllData"
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setData(data?.response);
+    } catch (error) {
+      console.log("Error occured " +error);
+    }
+  };
+
+  useEffect(() => {
+    DataTable();
+  }, []);
+
   return (
     <table>
       <thead>
         <tr>
-          <th>Name of Data</th>
+          <th>Name</th>
           <th>Type</th>
           <th>Status</th>
-          <th>Date</th>
+          <th>Link</th>
         </tr>
       </thead>
       <tbody>
@@ -18,7 +36,7 @@ const DataTable = ({ data }) => {
             <td>{item.name}</td>
             <td>{item.type}</td>
             <td>{item.status}</td>
-            <td>{item.date}</td>
+            <td>{item.link}</td>
           </tr>
         ))}
       </tbody>
@@ -26,4 +44,4 @@ const DataTable = ({ data }) => {
   );
 };
 
-export default DataTable;
+export default App;
